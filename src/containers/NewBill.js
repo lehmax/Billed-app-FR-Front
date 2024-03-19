3839cfbabadbb78294495206e4171fafb193abd2
Maxime Leherquer <45520008+lehmax@users.noreply.github.com>
2024-03-19 22:25:14 +0100

    fix(newBill): improve proccess on input file wrong file type

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,5 +1,8 @@
-import { ROUTES_PATH } from '../constants/routes.js'
+import { ROUTES_PATH } from "../constants/routes.js"
 import Logout from "./Logout.js"
+
+const isPicture = (mimeType) =>
+  ["image/jpeg", "image/jpg", "image/png", "image/gif"].includes(mimeType)
 
 export default class NewBill {
   constructor({ document, onNavigate, store, localStorage }) {
@@ -15,62 +18,91 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
-  handleChangeFile = e => {
+  handleChangeFile = (e) => {
     e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const inputFile = this.document.querySelector(`input[data-testid="file"]`)
+    const errorMessageElement = this.document.querySelector(`span[data-testid="error-msg"]`)
+    const file = inputFile.files[0]
+
+    if (errorMessageElement) {
+      errorMessageElement.remove()
+    }
+
+    if (!isPicture(file.type)) {
+      inputFile.file = null
+      inputFile.value = null
+      inputFile.setAttribute("aria-invalid", "true")
+      inputFile.setAttribute("aria-describedby", "fileError fileHint")
+
+      const errorMessage = `
+        <span data-testid="error-msg" class="error-msg">
+          <p id="fileError">Le format du fichier n'est pas valide</p>
+          <p id="fileHint">Formats acceptés : .jpg, .jpeg, .png, .gif</p>
+        </span>
+      `
+
+      inputFile.insertAdjacentHTML("afterend", errorMessage)
+
+      return
+    }
+
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const fileName = filePath[filePath.length - 1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    formData.append("file", file)
+    formData.append("email", email)
 
     this.store
       .bills()
       .create({
         data: formData,
         headers: {
-          noContentType: true
-        }
+          noContentType: true,
+        },
       })
-      .then(({fileUrl, key}) => {
+      .then(({ fileUrl, key }) => {
         console.log(fileUrl)
         this.billId = key
         this.fileUrl = fileUrl
         this.fileName = fileName
-      }).catch(error => console.error(error))
+      })
+      .catch((error) => console.error(error))
   }
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    console.log(
+      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
+      e.target.querySelector(`input[data-testid="datepicker"]`).value
+    )
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
-      status: 'pending'
+      status: "pending",
     }
     this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
+    this.onNavigate(ROUTES_PATH["Bills"])
   }
 
   // not need to cover this function by tests
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH["Bills"])
+        })
+        .catch((error) => console.error(error))
     }
   }
 }
